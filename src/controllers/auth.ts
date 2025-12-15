9416b9c4639c51fbaa7afbbe4876959b745ed0d3
SinethembaDlova <dsinethemba@gmail.com>
2025-12-16 01:54:05 +0200

    feat: created acuser response helper that exclude password.

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -4,3 +4,16 @@ import PasswordReset from '../models/PasswordReset';
 import { generateToken } from '../utils/jwt';
 import { sendPasswordResetEmail, sendWelcomeEmail } from '../services/email';
 
+// Helper to format user response (exclude password)
+const formatUserResponse = (user: any) => ({
+  id: user._id,
+  email: user.email,
+  firstName: user.firstName,
+  lastName: user.lastName,
+  phone: user.phone,
+  role: user.role,
+  address: user.address || {},
+  preferredPickupLocation: user.preferredPickupLocation || { usePrimaryAddress: true },
+  emailVerified: user.emailVerified,
+  createdAt: user.createdAt
+});
