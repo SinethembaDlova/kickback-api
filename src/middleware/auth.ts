47f446ae172f17cf3d308a75895556267d450d51
SinethembaDlova <dsinethemba@gmail.com>
2025-12-16 21:41:01 +0200

    feat: extended the express request to include the User

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -2,3 +2,11 @@ import { Request, Response, NextFunction } from 'express';
 import { ITokenPayload } from '../types/jwt';
 import { verifyToken } from '../utils/jwt';
 import User, { IUser } from '../models/User';
+
+// Extend Express Request type to include user
+declare module 'express-serve-static-core' {
+  interface Request {
+    user?: IUser;
+    tokenPayload?: ITokenPayload;
+  }
+}
